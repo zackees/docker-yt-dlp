e76b19ee493994b43ef6f0b9dd4da82ec356492f
zackees <z@zackees.com>
2024-02-18 17:41:54 -0800

    update

--- run.py
+++ run.py
@@ -74,9 +74,8 @@ def parse_args() -> tuple[argparse.Namespace, list[str]]:
     args, other_args = parser.parse_known_args()
     return args, other_args
 
-def main() -> None:
-    args, other_args = parse_args()
-    dockerfile = args.dockerfile
+
+def run(dockerfile: str, imagename: str, containername: str, hostvolume: str, remotevolume: str, cmd_args: list[str]) -> None:
     if not check_docker_running():
         start_docker_service()
     try:
@@ -85,22 +84,24 @@ def main() -> None:
         print("Docker is not running. Please start Docker and try again.")
         return
     
-    build_image(client, IMAGE_NAME, dockerfile=dockerfile)
-    if not image_exists(client, IMAGE_NAME):
+    build_image(client, imagename, dockerfile=dockerfile)
+    if not image_exists(client, imagename):
         print("Image does not exist, pulling...")
-        client.images.pull(IMAGE_NAME)
+        client.images.pull(imagename)
     
-    container: Optional[Container] = get_container(client, CONTAINER_NAME)
+    container: Optional[Container] = get_container(client, containername)
     if container:
         print("Removing existing container...")
         container.remove(force=True)  # Force removal if running or stopped
     
     print("Running Docker container with the necessary arguments...")
+    cmd_str = subprocess.list2cmdline(cmd_args)
+    volumes = {hostvolume: {'bind': remotevolume, 'mode': 'rw'}}
     container = client.containers.run(
-        IMAGE_NAME, 
-        " ".join(other_args),
-        name=CONTAINER_NAME,
-        volumes={HOST_VOLUME: {'bind': '/host_dir', 'mode': 'rw'}},
+        imagename, 
+        cmd_str,
+        name=containername,
+        volumes=volumes,
         detach=True,
         auto_remove=True
     )
@@ -108,6 +109,18 @@ def main() -> None:
     for log in container.logs(stream=True):
         print(log.decode("utf-8"), end="")
 
+def main() -> None:
+    args, other_args = parse_args()
+    dockerfile = args.dockerfile
+    run(dockerfile=dockerfile,
+        imagename=IMAGE_NAME,
+        containername=CONTAINER_NAME,
+        hostvolume=HOST_VOLUME,
+        remotevolume="/host_dir",
+        cmd_args=other_args)
+
 if __name__ == "__main__":
     sys.argv.append("Dockerfile")
+    sys.argv.append("--")
+    sys.argv.append("--version")
     main()
